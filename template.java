a4336ce7eaa8a68c60b01d67ab58a3ac62894d6e
Aksh Bansal <akshmark@gmail.com>
2021-03-12 13:24:37 +0530

    seive of eratosthenes

--- template.java
+++ template.java
@@ -46,10 +46,26 @@ public class Solution {
             return str;
         }
     }
-
+    private static boolean[] isPrime;
+    private static void primes(){
+        int num = (int)1e6; // PRIMES FROM 1 TO NUM
+        isPrime = new boolean[num];
+     
+        for (int i = 0; i< isPrime.length; i++) {
+           isPrime[i] = true;
+        }
+        for (int i = 2; i< Math.sqrt(num); i++) {
+           if(isPrime[i] == true) {
+              for(int j = (i*i); j<num; j = j+i) {
+                 isPrime[j] = false;
+              }
+           }
+        }
+    }
     public static void main(String[] args) throws IOException {
         FastReader sc = new FastReader();
         PrintWriter out = new PrintWriter(System.out);
+        // primes();
         // ________________________________
 
         int t = sc.nextInt();
