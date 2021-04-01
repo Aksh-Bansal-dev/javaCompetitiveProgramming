c7270721aa90e264f57b2b29a3530e4f2a8f035c
Aksh Bansal <akshmark@gmail.com>
2021-04-01 12:19:37 +0530

    minor bug fix

--- template.java
+++ template.java
@@ -51,7 +51,7 @@ public class Solution {
         int num = (int)1e6; // PRIMES FROM 1 TO NUM
         isPrime = new boolean[num];
      
-        for (int i = 0; i< isPrime.length; i++) {
+        for (int i = 2; i< isPrime.length; i++) {
            isPrime[i] = true;
         }
         for (int i = 2; i< Math.sqrt(num); i++) {
