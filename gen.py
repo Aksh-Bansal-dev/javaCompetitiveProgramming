8fba9aa477d721c1ac1350a3d271249b7a3b6cd1
Aksh Bansal <akshmark@gmail.com>
2021-06-05 17:29:58 +0530

    chore

--- gen.py
+++ gen.py
@@ -3,6 +3,6 @@ import random
 
 # b = sys.argv[1]
 print(1)
-print(10)
-for i in range(1, 11):
-    print(random.randint(1, 20), end=" ")
+print(100)
+for i in range(1, 101):
+    print(random.randint(1, 10000), end=" ")
