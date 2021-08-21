9a5b1fee2b52c29ddec42cb52c19cba2564f8a1f
Aksh Bansal <akshmark@gmail.com>
2021-08-21 14:12:31 +0530

    cleaner code

---
+++ resources/adjacencyList.java
@@ -0,0 +1,14 @@
+    static ArrayList<Integer>[] adj;
+    static void getAdj(int n,int q, FastReader sc){
+        adj = new ArrayList[n+1];
+        for(int i=1;i<=n;i++){
+            adj[i] = new ArrayList<>();
+        }
+        for(int i=0;i<q;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            adj[a].add(b);
+            adj[b].add(a);
+        }
+    }
--- template.java
+++ template.java
@@ -85,20 +85,7 @@ public class Solution {
             return temp*temp;
         }
     }
-    // static ArrayList<Integer>[] adj;
-    // static void getAdj(int n,int q, FastReader sc){
-    //     adj = new ArrayList[n+1];
-    //     for(int i=1;i<=n;i++){
-    //         adj[i] = new ArrayList<>();
-    //     }
-    //     for(int i=0;i<q;i++){
-    //         int a = sc.nextInt();
-    //         int b = sc.nextInt();
 
-    //         adj[a].add(b);
-    //         adj[b].add(a);
-    //     }
-    // }
     static PrintWriter out;
     static FastReader sc ;
     public static void main(String[] args) throws IOException {
