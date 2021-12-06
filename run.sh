f60598974cf8bc0fe8b7c5e580188363a1ca9ce7
Aksh Bansal <akshmark@gmail.com>
2021-12-06 21:55:16 +0530

    feat: improve run script (colors & binary files)
    - Print input along with output.
    - Add colors
    - Remove binary files after running script

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Colors
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo -e "${YELLOW}Input: ${NC}"
+cat input.txt
+echo -e "\n${YELLOW}Output: ${NC}"
+
 num="$#"
 if [[ "$num" = '0' ]]; then
     javac Solution.java && java Solution <input.txt  
@@ -15,8 +25,10 @@ do
     fileBasenameNoExtension="${filename%%.*}"
     if [ "${fileExtname}" = 'java' ]; then 
         javac ${filename} && java ${fileBasenameNoExtension} <input.txt; 
+        rm *.class;
     elif [ 'py' = "${fileExtname}" ]; then python3 ${filename} <input.txt; 
-    elif [ "${fileExtname}" = 'c' ] || [ "${fileExtname}" = 'cpp' ]; then g++ ${filename} && ./a.out <input.txt; 
+    elif [ "${fileExtname}" = 'c' ] || [ "${fileExtname}" = 'cpp' ]; then 
+        g++ -o tttt.out ${filename} && ./tttt.out <input.txt; 
+        rm tttt.out
     else echo 'File extension not supported'; fi
-    rm *.class
 done
