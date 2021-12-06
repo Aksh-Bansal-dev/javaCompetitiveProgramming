2f4571875733b6336b067e1f1ecf40e2358993a8
Aksh Bansal <akshmark@gmail.com>
2021-12-06 21:01:14 +0530

    feat: add min segTree and gcd segTree

--- resources/segmentTree.java
+++ resources/segmentTree.java
@@ -1,9 +1,8 @@
-	// Sum segment tree
+	// =================================== Sum segment tree ============================================
 	static class SegmentTree
 	{
 		long st[]; 
 		int n;
-
 	    SegmentTree(int arr[])
 		{
 			this.n = arr.length;
@@ -11,50 +10,40 @@
 			int max_size = 2 * (int) Math.pow(2, x) - 1; 
 			// max_size = 4*n
 			st = new long[max_size]; 
-
 			constructSTUtil(arr, 0, n - 1, 0);
 		}
-
 		int getMid(int s, int e) {
 			return s + (e - s) / 2;
 		}
-
 		long getSumUtil(int ss, int se, int qs, int qe, int si)
 		{
 			if (qs <= ss && qe >= se)
 				return st[si];
-
 			if (se < qs || ss > qe)
 				return 0;
-
 			int mid = getMid(ss, se);
 			return getSumUtil(ss, mid, qs, qe, 2 * si + 1) +
 					getSumUtil(mid + 1, se, qs, qe, 2 * si + 2);
 		}
-
 		void updateValueUtil(int ss, int se, int i, long diff, int si)
 		{
 			if (i < ss || i > se)
 				return;
-
-			st[si] = st[si] + diff;
+			st[si] = diff;
 			if (se != ss) {
 				int mid = getMid(ss, se);
 				updateValueUtil(ss, mid, i, diff, 2 * si + 1);
 				updateValueUtil(mid + 1, se, i, diff, 2 * si + 2);
 			}
 		}
-
 		void update(int i, long new_val)
 		{
 			if (i < 0 || i > n - 1) {
 				System.out.println(i+" Invalid Input u "+n);
 				return;
 			}
-
 			updateValueUtil(0, n - 1, i, new_val, 0);
 		}
-
 		long get(int qs, int qe)
 		{
 			if (qs < 0 || qe > n - 1 || qs > qe) {
@@ -63,14 +52,12 @@
 			}
 			return getSumUtil(0, n - 1, qs, qe, 0);
 		}
-
 		long constructSTUtil(int arr[], int ss, int se, int si)
 		{
 			if (ss == se) {
 				st[si] = arr[ss];
 				return arr[ss];
 			}
-
 			int mid = getMid(ss, se);
 			st[si] = constructSTUtil(arr, ss, mid, si * 2 + 1) +
 					constructSTUtil(arr, mid + 1, se, si * 2 + 2);
@@ -78,12 +65,81 @@
 		}
 	}
 
-	// Max segment tree
+
+	// =================================== GCD segment tree ============================================
+	static class SegmentTree
+	{
+		long st[]; 
+		int n;
+	    SegmentTree(int arr[])
+		{
+			this.n = arr.length;
+			int x = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+			int max_size = 2 * (int) Math.pow(2, x) - 1; 
+			// max_size = 4*n
+			st = new long[max_size]; 
+			constructSTUtil(arr, 0, n - 1, 0);
+		}
+		int getMid(int s, int e) {
+			return s + (e - s) / 2;
+		}
+		long getSumUtil(int ss, int se, int qs, int qe, int si)
+		{
+			if (qs <= ss && qe >= se)
+				return st[si];
+			if (se < qs || ss > qe)
+				return 0;
+			int mid = getMid(ss, se);
+			return gcd(getSumUtil(ss, mid, qs, qe, 2 * si + 1) ,
+					getSumUtil(mid + 1, se, qs, qe, 2 * si + 2));
+		}
+		void updateValueUtil(int ss, int se, int i, long diff, int si)
+		{
+			if (i < ss || i > se)
+				return;
+			if (se != ss) {
+				int mid = getMid(ss, se);
+				updateValueUtil(ss, mid, i, diff, 2 * si + 1);
+				updateValueUtil(mid + 1, se, i, diff, 2 * si + 2);
+			}
+			st[si] = diff;
+		}
+		void update(int i, long new_val)
+		{
+			if (i < 0 || i > n - 1) {
+				System.out.println(i+" Invalid Input u "+n);
+				return;
+			}
+			updateValueUtil(0, n - 1, i, new_val, 0);
+		}
+		long get(int qs, int qe)
+		{
+			if (qs < 0 || qe > n - 1 || qs > qe) {
+				System.out.println(qs+" Invalid Input g "+qe);
+				return -1;
+			}
+			return getSumUtil(0, n - 1, qs, qe, 0);
+		}
+		long constructSTUtil(int arr[], int ss, int se, int si)
+		{
+			if (ss == se) {
+				st[si] = arr[ss];
+				return arr[ss];
+			}
+			int mid = getMid(ss, se);
+			st[si] = gcd(constructSTUtil(arr, ss, mid, si * 2 + 1) ,
+					constructSTUtil(arr, mid + 1, se, si * 2 + 2));
+			return st[si];
+		}
+	}
+
+
+
+	// =================================== MIN segment tree ============================================
 	static class SegmentTree
 	{
 		long st[]; 
         int n;
-
 	    SegmentTree(int arr[])
 		{
             this.n = arr.length;
@@ -91,32 +147,99 @@
 			int max_size = 2 * (int) Math.pow(2, x) - 1; 
 			// max_size = 4*n
 			st = new long[max_size]; 
-
 			constructSTUtil(arr, 0, n - 1, 0);
 		}
-
 		int getMid(int s, int e) {
 			return s + (e - s) / 2;
 		}
-
 		long getSumUtil(int ss, int se, int qs, int qe, int si)
 		{
-			if (qs <= ss && qe >= se)
+			if (qs <= ss && qe >= se){
 				return st[si];
-
-			if (se < qs || ss > qe)
-				return -1;
-
+            }
+			if (se < qs || ss > qe){
+				return Integer.MAX_VALUE;
+            }
 			int mid = getMid(ss, se);
-			return Math.max(getSumUtil(ss, mid, qs, qe, 2 * si + 1) ,
+			return Math.min(getSumUtil(ss, mid, qs, qe, 2 * si + 1) ,
 					getSumUtil(mid + 1, se, qs, qe, 2 * si + 2));
 		}
-
 		void updateValueUtil(int ss, int se, int i, long diff, int si)
 		{
 			if (i < ss || i > se)
 				return;
+			if (se != ss) {
+				int mid = getMid(ss, se);
+				updateValueUtil(ss, mid, i, diff, 2 * si + 1);
+				updateValueUtil(mid + 1, se, i, diff, 2 * si + 2);
+                st[si] = Math.min(st[si*2+1], st[si*2+2]);
+			}
+            else{
+			    st[si] = diff;
+            }
+		}
+		void update( int i, long new_val)
+		{
+			if (i < 0 || i > n - 1) {
+				System.out.println("Invalid Input u"+i);
+				return;
+			}
+            // System.out.println(i+"__"+ new_val);
+			updateValueUtil(0, n - 1, i, new_val, 0);
+		}
+		long get(int qs, int qe)
+		{
+			if (qs < 0 || qe > n - 1 || qs > qe) {
+				System.out.println(qs+"Invalid Input g"+qe+" "+n);
+				return -1;
+			}
+			return getSumUtil(0, n - 1, qs, qe, 0);
+		}
+		long constructSTUtil(int arr[], int ss, int se, int si)
+		{
+			if (ss == se) {
+				st[si] = arr[ss];
+				return arr[ss];
+			}
+			int mid = getMid(ss, se);
+			st[si] = Math.min(constructSTUtil(arr, ss, mid, si * 2 + 1) ,
+					constructSTUtil(arr, mid + 1, se, si * 2 + 2));
+			return st[si];
+		}
+	}
 
+
+	// =================================== Max segment tree ============================================
+	static class SegmentTree
+	{
+		long st[]; 
+        int n;
+	    SegmentTree(int arr[])
+		{
+            this.n = arr.length;
+			int x = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+			int max_size = 2 * (int) Math.pow(2, x) - 1; 
+			// max_size = 4*n
+			st = new long[max_size]; 
+			constructSTUtil(arr, 0, n - 1, 0);
+		}
+		int getMid(int s, int e) {
+			return s + (e - s) / 2;
+		}
+		long getSumUtil(int ss, int se, int qs, int qe, int si)
+		{
+			if (qs <= ss && qe >= se)
+				return st[si];
+			if (se < qs || ss > qe)
+				return -1;
+			int mid = getMid(ss, se);
+			return Math.max(getSumUtil(ss, mid, qs, qe, 2 * si + 1) ,
+					getSumUtil(mid + 1, se, qs, qe, 2 * si + 2));
+		}
+		void updateValueUtil(int ss, int se, int i, long diff, int si)
+		{
+			if (i < ss || i > se)
+				return;
 			if (se != ss) {
 				int mid = getMid(ss, se);
 				updateValueUtil(ss, mid, i, diff, 2 * si + 1);
@@ -127,7 +250,6 @@
 			    st[si] = diff;
             }
 		}
-
 		void update( int i, long new_val)
 		{
 			if (i < 0 || i > n - 1) {
@@ -137,7 +259,6 @@
             // System.out.println(i+"__"+ new_val);
 			updateValueUtil(0, n - 1, i, new_val, 0);
 		}
-
 		long get(int qs, int qe)
 		{
 			if (qs < 0 || qe > n - 1 || qs > qe) {
@@ -146,14 +267,12 @@
 			}
 			return getSumUtil(0, n - 1, qs, qe, 0);
 		}
-
 		long constructSTUtil(int arr[], int ss, int se, int si)
 		{
 			if (ss == se) {
 				st[si] = arr[ss];
 				return arr[ss];
 			}
-
 			int mid = getMid(ss, se);
 			st[si] = Math.max(constructSTUtil(arr, ss, mid, si * 2 + 1) ,
 					constructSTUtil(arr, mid + 1, se, si * 2 + 2));
