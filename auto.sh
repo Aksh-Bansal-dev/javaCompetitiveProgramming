e16ed06efa9e950ca2986133e5d3b051f2f8c94a
Aksh Bansal <akshmark@gmail.com>
2021-12-11 20:09:06 +0530

    feat: stress testing for constructive solutions

--- auto.sh
+++ auto.sh
@@ -8,28 +8,62 @@ NC='\033[0m' # No Color
 
 javac Gen.java
 javac Solution.java
-javac Brute.java
 
-testcase=0
+if [[ $# -eq 1  ]] 
+then 
+    # Compile common files
 
-while [[ 1 -lt 2 ]]
-do 
-    testcase=$((testcase+1))
-    java Gen > input.txt
-    java Brute <input.txt>output2.txt
-    java Solution<input.txt>output.txt
-    diff -q output.txt output2.txt
-    if [[ $? -eq 1 ]] 
+    # Constructive solution(Solution with multiple correct answer)  
+    # Match word -> 'Wrong!'
+    if [[ $1 = '-c' ]]
     then
-        echo "Testcase: $testcase"
-        echo -e "${YELLOW}Input: ${NC}"
-        cat input.txt
-        echo -e "\n${GREEN}Correct Output:${NC}"
-        cat output2.txt
-        echo -e "${RED}Your Output:${NC}"
-        cat output.txt
-        break
-    fi
-done
+        testcase=0
+        while [[ 1 -lt 2 ]]
+        do 
+            testcase=$((testcase+1))
+            java Gen > input.txt
+            java Solution<input.txt>output.txt
 
-rm *.class output2.txt
+            if grep -q 'Wrong!' output.txt; then
+                echo "Testcase: $testcase"
+                echo -e "${YELLOW}Input: ${NC}"
+                cat input.txt
+                echo -e "\n${RED}Output:${NC}"
+                cat output.txt
+                break
+            fi
+        done
+
+        rm *.class
+        exit 0
+    else
+        echo "Bad arguments"
+        exit 1
+    fi
+else
+    # Solution with single correct answer
+    javac Brute.java
+    testcase=0
+    while [[ 1 -lt 2 ]]
+    do 
+        testcase=$((testcase+1))
+        java Gen > input.txt
+        java Brute <input.txt>output2.txt
+        java Solution<input.txt>output.txt
+        diff -q output.txt output2.txt
+        if [[ $? -eq 1 ]] 
+        then
+            echo "Testcase: $testcase"
+            echo -e "${YELLOW}Input: ${NC}"
+            cat input.txt
+            echo -e "\n${GREEN}Correct Output:${NC}"
+            cat output2.txt
+            echo -e "${RED}Your Output:${NC}"
+            cat output.txt
+            break
+        fi
+    done
+
+    rm *.class output2.txt
+
+fi
