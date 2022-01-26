fa97015f6f7ed54c5b5627a1e19a6fc4230070be
Aksh Bansal <akshmark@gmail.com>
2022-01-26 18:50:09 +0530

    feat: fermits little nCr

--- resources/ncr.java
+++ resources/ncr.java
@@ -1,3 +1,36 @@
+    // Returns nCr % p using Fermat's
+    // little theorem.
+    static long modInverse(long n, long p)
+    {
+        return pow(n, p - 2);
+    }
+ 
+    static long ncr(int n, int r, int p)
+    {
+ 
+          if (n<r)
+              return 0;
+        // Base case
+        if (r == 0)
+            return 1;
+ 
+        // ==============================
+        // Calculate this only once
+        static long[] fac;
+ 
+        fac = new long[n + 1];
+        fac[0] = 1;
+ 
+        for (int i = 1; i <= n; i++)
+            fac[i] = (fac[i - 1] * i) % mod;
+        // ============================
+
+        return (fac[n] * modInverse(fac[r], p)
+                % p * modInverse(fac[n - r], p)
+                % p)
+            % p;
+    }
+
     static long[][] memNcr; 
     static long ncr(int n, int r)
     {
@@ -9,3 +42,5 @@
         memNcr[n][r] = res;
         return res;
     }
+
+
