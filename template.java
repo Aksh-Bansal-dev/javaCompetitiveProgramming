582bedc9f2da0faf236832834b90c075d54bed0e
Aksh Bansal <akshmark@gmail.com>
2022-07-14 18:35:11 +0530

    chore: remove unnecessary code

--- template.java
+++ template.java
@@ -79,10 +79,6 @@ public class Solution {
 
             solver();
         }
-        // ================================ //
-        // int n = sc.nextInt();
-        // solver();
-        // ================================ //
         out.flush();
     }
 
